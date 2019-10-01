685fdf3741d97bfea5df34c024171a3568f65209
HobsonMC2105 <56050416+HobsonMC2105@users.noreply.github.com>
2019-10-01 14:57:57 -0700

    Finished game
    The game is now finished with some adjustments

--- rps.py
+++ rps.py
@@ -1,4 +1,6 @@
 #mathew hobson
+#added comment for github
+
 import random
 pScore = 0
 cScore = 0
@@ -10,7 +12,7 @@ def printscore():
 	print("Score: ")
 	print(pname + ":" + str(pScore))
 	print("Computer score:" + str(cScore))
-	print("Ties: " + str(cScore))
+	print("Ties: " + str(ties))
 
 
 print("Welcome to rock paper scissors")
@@ -19,19 +21,17 @@ pname = input("What is your name? ")
 while True:
 	pMove = input("What is your move?:'r' rock, 'p' paper, 's' scissors, or quit 'q' ")
 
-	printscore()
-
 	if pMove == 'q':
 		break
 	cMove = random.choice(cMoves)
 
 	if pMove == "r":
-		print(pname + "picked rock")
-		if cMoves == "r": 
+		print(pname + " picked rock")
+		if cMove == "r": 
 			print("Computer picks rock")
 			print("tie")
 			ties += 1
-		elif cMove == "p"
+		elif cMove == "p":
 			print("Computer pick Paper")
 			print("Paper covers rock")
 			cScore += 1
@@ -39,11 +39,38 @@ while True:
 			print("Computer picked scissors")
 			print("rock breaks scissors")
 			pScore += 1
-	elif pMove == "p"
-		pass
+	elif pMove == "p":
+		print(pname + " picked paper")
+		if cMove == "r": 
+			print("Computer picks rock")
+			print("Paper covers rock")
+			pScore += 1
+		elif cMove == "p":
+			print("Computer pick Paper")
+			print("tie")
+			ties += 1
+		else:
+			print("Computer picked scissors")
+			print("Scissors cuts paper")
+			cScore += 1
 
-	elif pMove == "s"
+	elif pMove == "s":
+		print(pname + " picked scissors")
+		if cMove == "r": 
+			print("Computer picks rock")
+			print("Rock breaks scissors")
+			cScore += 1
+		elif cMove == "p":
+			print("Computer pick Paper")
+			print("Scissors cuts paper")
+			pMove += 1
+		else:
+			print("Computer picked scissors")
+			print("tie")
+			ties += 1
 
 	else:
 		print("not an option")
+
+	printscore()
 
